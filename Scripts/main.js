d20726994629820275154139fdb11565633d8dee
Xavier Marleau <xavier.marleau@usherbrooke.ca>
2015-02-18 23:36:22 -0500

    changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -9,6 +9,14 @@
 ga('create', 'UA-59930311-1', 'auto');
 ga('send', 'pageview');
 
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 function moveToSection(id) {
     $('html, body').animate({ scrollTop: $(id).offset().top - $("#menu").height()}, 800);
 };
